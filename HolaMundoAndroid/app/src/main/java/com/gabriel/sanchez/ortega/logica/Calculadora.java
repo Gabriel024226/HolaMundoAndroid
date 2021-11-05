fc259e21cf65c209f29e07c3e00eb946b7f676b1
Gabriel024226 <sanchez.ortega.gabriel024226@gmail.com>
2021-11-05 09:38:30 -0600

    tercera version

---
+++ HolaMundoAndroid/app/src/main/java/com/gabriel/sanchez/ortega/logica/Calculadora.java
@@ -0,0 +1,23 @@
+package com.gabriel.sanchez.ortega.logica;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable
+{
+    public void suma()
+    {
+
+    }
+    public void resta()
+    {
+
+    }
+    public void multiplicacion()
+    {
+
+    }
+    public void division()
+    {
+
+    }
+}
