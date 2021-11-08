c96e0fa9b5669acb66948df10ff700dfe357a8b2
Gabriel024226 <sanchez.ortega.gabriel024226@gmail.com>
2021-11-08 11:09:32 -0600

    Primera version

---
+++ HolaMundoAndroid2/app/src/main/java/com/gabriel/sanchez/holamundoandroid2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.gabriel.sanchez.holamundoandroid2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBorrar(View view) {
+    }
+}
